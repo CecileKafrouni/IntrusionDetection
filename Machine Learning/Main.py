af5a105a95443b8904cbf528f05ebc693f9fc43a
TheodoreLerondeau <50512126+TheodoreLerondeau@users.noreply.github.com>
2020-11-24 18:28:38 +0100

    Update Main.py

--- Machine Learning/Main.py
+++ Machine Learning/Main.py
@@ -8,23 +8,24 @@
 import pandas as pd
 
 import Preparation_CSV as pc        
-#import feature_importances_ as fi
+import feature_importances as fi
 import Normalisation_donnees as nor
 
-df_malicious = pd.read_csv("l2-malicious.csv")
-df_benign = pd.read_csv("l2-benign.csv")
+
+df_malicious = pd.read_csv("data/l2-malicious.csv")
+df_benign = pd.read_csv("data/l2-benign.csv")
 
 df_total_csv = pd.concat([df_malicious,df_benign])
-df_total_csv = df_total_csv.reset_index(drop = True)
 
 # Preparation des données
 df_total_csv_new = pc.Preparation_CSV(df_total_csv)
-
+#df_total_csv_new = df_total_csv.reset_index(drop = True)
 # On met notre nouvelle dataframe dans un fichier csv
 df_total_csv_new.to_csv("total_csv_copy.csv", sep=';', index=False)
 
 # On normalise les données pour les modèles de ML au cas ou
 df_total_csv_normalisee = nor.NormalizeDataset(df_total_csv_new)
-
 df_total_csv_normalisee.to_csv("df_total_csv_normalisee.csv", sep=';', index=False)
 
+df_feature_importances = fi.FeaturesImportances(df_total_csv_normalisee)
+df_feature_importances.to_csv("df_feature_importances.csv", sep=';', index=False)
