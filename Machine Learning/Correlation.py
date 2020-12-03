0c8c4ad386b1b3883db9c7a3d207ba6a7042b16c
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2020-12-03 19:40:33 +0100

    Update Correlation.py

--- Machine Learning/Correlation.py
+++ Machine Learning/Correlation.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
-
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def Correlation(df):
+def Correlation(df, colonne):
     #X = df.drop(['Intrusion','SourceIP','DestinationIP'], axis = 1)
     #y = df['Intrusion']
     #X.astype(np.float32)
@@ -18,15 +16,15 @@ def Correlation(df):
     plt.show()
     
     #Correlation with output variable
-    cor_target = abs(cor["Intrusion"]) #Selecting highly correlated features
+    cor_target = abs(cor[colonne]) #Selecting highly correlated features
     
     relevant_features = cor_target[cor_target>0.1]
      
     df_copy = pd.DataFrame()
-    for colonne in df.columns:
+    for nom_colonne in df.columns:
         for index, value in relevant_features.items():
-            if(colonne == index):
-                df_copy[colonne] = df[colonne]
+            if(nom_colonne == index):
+                df_copy[nom_colonne] = df[nom_colonne]
                 
     return df_copy
     
