f96ab82a291bf8b4f90beb38bdc5559d359db2ba
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2020-12-03 19:40:41 +0100

    Update Main.py

--- Machine Learning/Main.py
+++ Machine Learning/Main.py
@@ -6,30 +6,122 @@
 '''
 
 import pandas as pd
+import pickle
 
 import Preparation_CSV as pc        
 import feature_importances as fi
 import Normalisation_donnees as nor
 import Correlation as cor
+import Modeles_ML as ml
+
+def Main():
+    reponse = input('Bonjour, que voulez vous tester ? \n A. DoH \n B. Intrusion \n\n')
+    
+    if(reponse == 'A'):
+        print('\nVous avez répondu A, c\'est parti pour DoH\n')
+        colonne = 'DoH'
+        
+        df_doh = pd.read_csv("data/l1-doh.csv")
+        df_nondoh = pd.read_csv("data/l1-nondoh.csv")
+    
+        df_total_csv_DoH = pd.concat([df_nondoh,df_doh])
+    
+        # Preparation des données
+        df_total_csv_new_DoH = pc.Preparation_CSV(df_total_csv_DoH, colonne)
+        df_total_csv_new_DoH.to_csv("DoH/total_csv_copy_DoH.csv", sep=';', index=False)
+        
+        # On normalise les données pour les modèles de ML au cas ou
+        df_total_csv_normalisee_DoH = nor.NormalizeDataset(df_total_csv_new_DoH)
+        df_total_csv_normalisee_DoH.to_csv("DoH/df_total_csv_normalisee_DoH.csv", sep=';', index=False)
+        
+        # Features importances
+        df_feature_importances_DoH = fi.FeaturesImportances(df_total_csv_normalisee_DoH, colonne)
+        df_feature_importances_DoH.to_csv("DoH/df_feature_importances_DoH.csv", sep=';', index=False)
+        
+        # Correlation
+        df_correlation_DoH = cor.Correlation(df_total_csv_normalisee_DoH, colonne)
+        df_correlation_DoH.to_csv("DoH/df_correlation_DoH.csv", sep=';', index=False)
+        
+        # Modele ML
+        # DTC Decision Tree Classifier
+        DTC_DoH = ml.DTC(df_total_csv_normalisee_DoH, colonne)
+        filename_DTC_DoH = 'DoH/finalized_model_DTC_DoH.sav'
+        pickle.dump(DTC_DoH, open(filename_DTC_DoH, 'wb'))
+        
+        # RFC Random Forest Classifier
+        RFC_DoH = ml.RFC(df_total_csv_normalisee_DoH, colonne)
+        filename_RFC_DoH = 'DoH/finalized_model_RFC_DoH.sav'
+        pickle.dump(RFC_DoH, open(filename_RFC_DoH, 'wb'))
+        
+        # XGB XGBoost Classifier
+        XGB_DoH = ml.XGB(df_total_csv_normalisee_DoH, colonne)
+        filename_XGB_DoH = 'DoH/finalized_model_XGB_DoH.sav'
+        pickle.dump(XGB_DoH, open(filename_XGB_DoH, 'wb'))
+        
+    elif(reponse == 'B'):
+        print('\nVous avez répondu B, c\'est parti pour Intrusion\n')
+        colonne = 'Intrusion'
+        
+        df_malicious = pd.read_csv("data/l2-malicious.csv")
+        df_benign = pd.read_csv("data/l2-benign.csv")
+    
+        df_total_csv_Intrusion = pd.concat([df_malicious,df_benign])
+    
+        # Preparation des données
+        df_total_csv_new_Intrusion = pc.Preparation_CSV(df_total_csv_Intrusion, colonne)
+        df_total_csv_new_Intrusion.to_csv("Intrusion/total_csv_copy_Intrusion.csv", sep=';', index=False)
+        
+        # On normalise les données pour les modèles de ML au cas ou
+        df_total_csv_normalisee_Intrusion = nor.NormalizeDataset(df_total_csv_new_Intrusion)
+        df_total_csv_normalisee_Intrusion.to_csv("Intrusion/df_total_csv_normalisee_Intrusion.csv", sep=';', index=False)
+        
+        # Features importances
+        df_feature_importances_Intrusion = fi.FeaturesImportances(df_total_csv_normalisee_Intrusion, colonne)
+        df_feature_importances_Intrusion.to_csv("Intrusion/df_feature_importances_Intrusion.csv", sep=';', index=False)
+    
+        # Correlation
+        df_correlation_Intrusion = cor.Correlation(df_total_csv_normalisee_Intrusion, colonne)
+        df_correlation_Intrusion.to_csv("Intrusion/df_correlation_Intrusion.csv", sep=';', index=False)
+        
+        # Modele ML
+        # DTC Decision Tree Classifier
+        DTC_Intrusion = ml.DTC(df_total_csv_normalisee_Intrusion, colonne)
+        filename_DTC_Intrusion = 'Intrusion/finalized_model_DTC_Intrusion.sav'
+        pickle.dump(DTC_Intrusion, open(filename_DTC_Intrusion, 'wb'))
+        
+        # RFC Random Forest Classifier
+        RFC_Intrusion = ml.RFC(df_total_csv_normalisee_Intrusion, colonne)
+        filename_RFC_Intrusion = 'Intrusion/finalized_model_RFC_Intrusion.sav'
+        pickle.dump(RFC_Intrusion, open(filename_RFC_Intrusion, 'wb'))
+        
+        # XGB XGBoost Classifier
+        XGB_Intrusion = ml.XGB(df_total_csv_normalisee_Intrusion, colonne)
+        filename_XGB_Intrusion = 'Intrusion/finalized_model_XGB_Intrusion.sav'
+        pickle.dump(XGB_Intrusion, open(filename_XGB_Intrusion, 'wb'))
+        
+    else:
+        print('\nVous n\' avez pas bien répondu à la question, réessayez svp')
+        Main()
 
 
-df_malicious = pd.read_csv("data/l2-malicious.csv")
+# On appelle notre fonction main
+Main()
+
+
+
+# -----------------------------------------------------------------------------------
+'''
+import re
+ 
+i=0
 df_benign = pd.read_csv("data/l2-benign.csv")
+for value in df_benign['SourceIP'].values:
+    charsToRemove = ["."]
+    df_benign['SourceIP'][i]=re.sub("|".join([re.escape(x) for x in charsToRemove]), "", value)
+    print(re.sub("|".join([re.escape(x) for x in charsToRemove]), "", value))
+    i+=1
 
-df_total_csv = pd.concat([df_malicious,df_benign])
+print(df_benign['SourceIP'].str.split('.'))
+# et apres mettre dans 4colonnes 
 
-# Preparation des données
-df_total_csv_new = pc.Preparation_CSV(df_total_csv)
-#df_total_csv_new = df_total_csv.reset_index(drop = True)
-# On met notre nouvelle dataframe dans un fichier csv
-df_total_csv_new.to_csv("total_csv_copy.csv", sep=';', index=False)
-
-# On normalise les données pour les modèles de ML au cas ou
-df_total_csv_normalisee = nor.NormalizeDataset(df_total_csv_new)
-df_total_csv_normalisee.to_csv("df_total_csv_normalisee.csv", sep=';', index=False)
-
-df_feature_importances = fi.FeaturesImportances(df_total_csv_normalisee)
-df_feature_importances.to_csv("df_feature_importances.csv", sep=';', index=False)
-
-df_correlation = cor.Correlation(df_total_csv_normalisee)
-df_correlation.to_csv("df_correlation.csv", sep=';', index=False)
+'''
