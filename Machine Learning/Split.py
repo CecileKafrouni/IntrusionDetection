0a6ae277f836f76aafb6e4c5cf35e2276f0c20c0
TheodoreLerondeau <50512126+TheodoreLerondeau@users.noreply.github.com>
2020-12-08 20:17:07 +0100

    Update Split.py

--- Machine Learning/Split.py
+++ Machine Learning/Split.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 def splitIp(df, column_to_split):
     df[[column_to_split+'_1',column_to_split+'_2',column_to_split+'_3', column_to_split+'_4']] = df[column_to_split].str.split(".",expand=True,)
+    df[column_to_split+'_1'] = pd.to_numeric(df[column_to_split+'_1'])
+    df[column_to_split+'_2'] = pd.to_numeric(df[column_to_split+'_2'])
+    df[column_to_split+'_3'] = pd.to_numeric(df[column_to_split+'_3'])
+    df[column_to_split+'_4'] = pd.to_numeric(df[column_to_split+'_4'])
 
     del df[column_to_split]
-    '''
-    df.insert(column_index, df[column_to_split+'_1'], int)
-    df.insert(column_index+1, df[column_to_split+'_2'], int)
-    df.insert(column_index+2, df[column_to_split+'_3'], int)
-    df.insert(column_index+3, df[column_to_split+'_4'], int)
-'''
     return df
+
+
