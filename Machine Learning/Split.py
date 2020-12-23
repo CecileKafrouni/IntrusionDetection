7a0483be2011ced9121ca166ecb6debe3d8d6a8f
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2020-12-23 11:13:37 +0100

    Update Split.py

--- Machine Learning/Split.py
+++ Machine Learning/Split.py
@@ -13,3 +13,21 @@ def splitIp(df, column_to_split):
     return df
 
 
+def IP2Int(df,column):
+    i=0
+    for values in df[column].values:
+        o = list(map(int, values.split('.')))
+        res = (16777216 * o[0]) + (65536 * o[1]) + (256 * o[2]) + o[3]
+        df[column][i] = res
+        i+=1
+    return df
+
+
+def Int2IP(ipnum):
+    o1 = int(ipnum / 16777216) % 256
+    o2 = int(ipnum / 65536) % 256
+    o3 = int(ipnum / 256) % 256
+    o4 = int(ipnum) % 256
+    return '%(o1)s.%(o2)s.%(o3)s.%(o4)s' % locals()
+
+
