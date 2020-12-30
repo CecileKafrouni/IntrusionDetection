7e3e3977cb1d5f06a91215e16fa7f90cd6bfe36c
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2020-12-30 16:09:22 +0100

    Update Main_interface.py

--- Machine Learning/Main_interface.py
+++ Machine Learning/Main_interface.py
@@ -1,9 +1,6 @@
-# -*- coding: utf-8 -*-
-
 '''
 ----------------------- MAIN INTERFACE ---------------------------
 '''
-
 
 import Modeles_ML as ml
 import Interface as gui
@@ -12,6 +9,7 @@ import pandas as pd
 
 df = pd.read_csv("DoH/df_total_csv_normalisee_DoH.csv", sep=';')
 
+#Loading ML models for DoH
 
 filename_DTC_DoH = 'DoH/finalized_model_DTC_DoH.sav'
 filename_RFC_DoH = 'DoH/finalized_model_RFC_DoH.sav'
@@ -21,18 +19,34 @@ loaded_model_DTC_DoH = pickle.load(open(filename_DTC_DoH, 'rb'))
 loaded_model_RFC_DoH = pickle.load(open(filename_RFC_DoH, 'rb'))
 loaded_model_XGB_DoH = pickle.load(open(filename_XGB_DoH, 'rb'))
 
+#Loading DL models for Intrusion
+
+filename_DTC_Intrusion = 'Intrusion/finalized_model_DTC_Intrusion.sav'
+filename_RFC_Intrusion = 'Intrusion/finalized_model_RFC_Intrusion.sav'
+filename_XGB_Intrusion = 'Intrusion/finalized_model_XGB_Intrusion.sav'
+
+loaded_model_DTC_Intrusion = pickle.load(open(filename_DTC_Intrusion, 'rb'))
+loaded_model_RFC_Intrusion = pickle.load(open(filename_RFC_Intrusion, 'rb'))
+loaded_model_XGB_Intrusion = pickle.load(open(filename_XGB_Intrusion, 'rb'))
+
 df_test=gui.interface(df,'DoH')
 
 df_test = pd.read_csv("df_new.csv", sep=';')
+
+
 pred_DTC_DoH = ml.DTC_Prediction(df_test, loaded_model_DTC_DoH)
-pred_RFC_DoH = ml.RFC_Prediction(df_test, loaded_model_RFC_DoH)
 pred_XGB_DoH = ml.XGB_Prediction(df_test, loaded_model_XGB_DoH)
+pred_RFC_DoH = ml.RFC_Prediction(df_test, loaded_model_RFC_DoH)
+
 
 gui.result(pred_DTC_DoH,pred_RFC_DoH,pred_XGB_DoH)
 
 
 
 
-filename_DTC_Intrusion = 'Intrusion/finalized_model_DTC_Intrusion.sav'
-filename_RFC_Intrusion = 'Intrusion/finalized_model_RFC_Intrusion.sav'
-filename_XGB_Intrusion = 'Intrusion/finalized_model_XGB_Intrusion.sav'
+pred_DTC_Intrusion = ml.DTC_Prediction(df_test, loaded_model_DTC_Intrusion)
+pred_RFC_Intrusion = ml.RFC_Prediction(df_test, loaded_model_RFC_Intrusion)
+pred_XGB_Intrusion = ml.XGB_Prediction(df_test, loaded_model_XGB_Intrusion)
+
+gui.result_intrusion(pred_DTC_Intrusion,pred_RFC_Intrusion,pred_XGB_Intrusion)
+
