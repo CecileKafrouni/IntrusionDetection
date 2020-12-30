293e8a6a48f2bab1b942d5eb5dacded7dcf75467
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2020-12-30 16:09:18 +0100

    Update Interface.py

--- Machine Learning/Interface.py
+++ Machine Learning/Interface.py
@@ -14,27 +14,20 @@ def interface(df, target):
     
     sg.theme('DarkBlue3')   
     
-    left_col = []
+    frame_layout = []
     right_col = []
     #left_col.append([sg.Text('Veuillez rentrer vos informations :')])
         
     #for i in range(0,len(liste_colonne)-1):
-    for i in range(0,int(len(liste_colonne)/2)+1):
-        left_col.append([sg.Text(liste_colonne[i], size=(30, 1)), 
+    for i in range(0,len(liste_colonne)):
+        frame_layout.append([sg.Text(liste_colonne[i], size=(30, 1)), 
                        sg.InputText(default_text = 0,size=(10,1))])
-     
-        
-    for i in range(int(len(liste_colonne)/2)+1,len(liste_colonne)):
-        right_col.append([sg.Text(liste_colonne[i], size=(30, 1)), 
-                       sg.InputText(default_text = 0,size=(10,1))])
-        
-    
-    frame_layout = [
-                    [sg.Column(left_col, element_justification='c'), 
-                     sg.VerticalSeparator(pad=None),
-                     sg.Column(right_col, element_justification='c')]
-                    ]
 
+    frame_layout = [
+                    [sg.Column(frame_layout[0:int(len(liste_colonne)/2)+1], element_justification='c'), 
+                     sg.VerticalSeparator(pad=None),
+                     sg.Column(frame_layout[int(len(liste_colonne)/2)+1:len(liste_colonne)], element_justification='c')]
+                    ]
     #frame_layout.append([sg.Button('Ok'), sg.Button('Cancel')])
 
     layout = [
@@ -44,17 +37,15 @@ def interface(df, target):
    
     
     window = sg.Window('Informations pour prediction', layout, size=(750,520))
-    event, values = window.read()
-    while (event != 'Ok'):
+    while (True):
         
+        event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Cancel' : 
             break
        
         X_new = []
-        for i in range (0, len(left_col)):
-            X_new.append(values[i])
-        for i in range (0, len(right_col)):
-            X_new.append(values[i])
+        for j in range (0, len(liste_colonne)):
+            X_new.append(values[j])
         print(X_new)
     
     features=[]
@@ -77,17 +68,20 @@ if __name__ == "__main__":
 def result(pred_DTC_DoH,pred_RFC_DoH, pred_XGB_DoH):
     
     if(pred_DTC_DoH == 1.0):
-        resultat_DTC_DoH = 'nonDoH'
+        #resultat_DTC_DoH = 'nonDoH'
+        result_intrusion()
     else:
         resultat_DTC_DoH = 'DoH'
     
     if(pred_RFC_DoH == 1.0):
-        resultat_RFC_DoH = 'nonDoH'
+        #resultat_RFC_DoH = 'nonDoH'
+        result_intrusion()
     else:
         resultat_RFC_DoH = 'DoH'
-    
+            
     if(pred_XGB_DoH == 1.0):
-        resultat_XGB_DoH = 'nonDoH'
+        #resultat_XGB_DoH = 'nonDoH'
+        result_intrusion()
     else:
         resultat_XGB_DoH = 'DoH'
        
@@ -95,4 +89,23 @@ def result(pred_DTC_DoH,pred_RFC_DoH, pred_XGB_DoH):
              'Le resultat pour le RFC :{}'.format(resultat_RFC_DoH), 
              'Le resultat pour le XGB :{}'.format(resultat_XGB_DoH))
     
+def result_intrusion(pred_DTC_Intrusion, pred_RFC_Intrusion, pred_XGB_Intrusion):
     
+    if(pred_DTC_Intrusion == 1.0):
+        resultat_DTC_Intrusion = 'Intrusion'
+    else:
+        resultat_DTC_Intrusion = 'Begnin'
+    
+    if(pred_RFC_Intrusion == 1.0):
+        resultat_RFC_Intrusion = 'Intrusion'
+    else:
+        resultat_RFC_Intrusion = 'Begnin'
+    
+    if(pred_XGB_Intrusion == 1.0):
+        resultat_XGB_Intrusion = 'Intrusion'
+    else:
+        resultat_XGB_Intrusion = 'Begnin'
+        
+    sg.popup('Resultat', 'Le resultat pour le DTC :{}'.format(resultat_DTC_Intrusion),
+             'Le resultat pour le RFC :{}'.format(resultat_RFC_Intrusion), 
+             'Le resultat pour le XGB :{}'.format(resultat_XGB_Intrusion))
