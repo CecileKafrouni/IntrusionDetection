220a93759583060e46f67681c8b2ad50497bede8
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2021-01-06 11:41:38 +0100

    Update feature_importances.py

--- Machine Learning/feature_importances.py
+++ Machine Learning/feature_importances.py
@@ -1,13 +1,11 @@
+# -*- coding: utf-8 -*-
+
 '''
 ------------------------------ Features Importances -----------------------------------
 '''
-import ipaddress
+
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
-from functools import reduce
 
 def FeaturesImportances(df, colonne, model, model_name):
     
