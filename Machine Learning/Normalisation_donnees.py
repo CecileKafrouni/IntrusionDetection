9d675f18680c3a8936a13a82dfce5f0e072b51eb
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2021-01-06 11:42:03 +0100

    Update Normalisation_donnees.py

--- Machine Learning/Normalisation_donnees.py
+++ Machine Learning/Normalisation_donnees.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-''' -------------------- Normalisation des données -------------------------------- '''
+'''
+-------------------- Normalisation des données -------------------------------- 
+'''
 
-import pandas as pd
 import numpy as np
+
 def normalize(colonne):
     max_value = colonne.max()
     min_value = colonne.min()
@@ -15,6 +17,4 @@ def NormalizeDataset(dataset) :
         if(dataset[colonne].dtypes == 'float64' or dataset[colonne].dtypes == 'int64' and colonne != 'SourcePort' and colonne != 'DestinationPort'):
            dataset[colonne] = normalize(dataset[colonne])
            
-    #dataset_normalise = normalize(dataset.iloc[:,2:len(dataset)])
-    #dataset_final_normalise = pd.concat([dataset.iloc[:,0:2], dataset_normalise, dataset.iloc[:,len(dataset):]], axis=1)
     return dataset
