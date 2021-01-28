e992a28e6d04dba9b62536abf5b236cbdc11d85b
TheodoreLerondeau <50512126+TheodoreLerondeau@users.noreply.github.com>
2021-01-28 18:21:35 +0100

    Update cross_validation.py

--- Machine Learning/cross_validation.py
+++ Machine Learning/cross_validation.py
@@ -5,12 +5,16 @@
 '''
 
 from sklearn.model_selection import cross_val_score
+from imblearn.over_sampling import SMOTE
 
 def cross_validation(df, colonne, model, model_name):
     
     X = df.drop([colonne], axis = 1)
     y = df[colonne]
     
-    scores = cross_val_score(model, X, y, cv=5)
+    oversample = SMOTE()
+    X_smote, y_smote = oversample.fit_sample(X, y)
+    
+    scores = cross_val_score(model, X_smote, y_smote, cv=5)
 
     print("\nAccuracy for " + model_name + ": %0.2f (+/- %0.2f)\n\n" % (scores.mean(), scores.std() * 2))
