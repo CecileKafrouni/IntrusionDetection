fbd0b8d35341f4ece7bd12eacc3e75cb16519e05
TheodoreLerondeau <50512126+TheodoreLerondeau@users.noreply.github.com>
2021-01-28 18:21:38 +0100

    Update feature_importances.py

--- Machine Learning/feature_importances.py
+++ Machine Learning/feature_importances.py
@@ -6,33 +6,39 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
+
 
 def FeaturesImportances(df, colonne, model, model_name):
     
     X = df.drop([colonne], axis = 1)
     y = df[colonne]
-    model_FI = model.fit(X,y)
     
-    importances1 = pd.Series(index=X.columns[:13],
+    oversample = SMOTE()
+    X_smote, y_smote = oversample.fit_sample(X, y)
+    
+    model_FI = model.fit(X_smote, y_smote)
+    
+    importances1 = pd.Series(index=X_smote.columns[:13],
                             data=model_FI.feature_importances_[:13])
     importances_sorted1 = importances1.sort_values()
     importances_sorted1.plot(kind='barh', color='red', title= model_name +'- Features importance 1/3')
     plt.show()
     
-    importances2 = pd.Series(index=X.columns[13:26],
+    importances2 = pd.Series(index=X_smote.columns[13:26],
                             data=model_FI.feature_importances_[13:26])
     importances_sorted2 = importances2.sort_values()
     importances_sorted2.plot(kind='barh', color='red', title=model_name +'- Features importance 2/3')
     plt.show()
     
-    importances3 = pd.Series(index=X.columns[26:],
+    importances3 = pd.Series(index=X_smote.columns[26:],
                             data=model_FI.feature_importances_[26:])
     importances_sorted3 = importances3.sort_values()
     importances_sorted3.plot(kind='barh', color='red', title=model_name +'- Features importance 3/3')
     plt.show()
     
     
-    feature_importances = pd.DataFrame(model_FI.feature_importances_, index = X.columns, columns=[colonne]).sort_values(colonne,ascending=False)
+    feature_importances = pd.DataFrame(model_FI.feature_importances_, index = X_smote.columns, columns=[colonne]).sort_values(colonne,ascending=False)
     
     df_copy = df.copy()
     
