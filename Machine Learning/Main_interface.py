55338aee93349f7e048072911613abb34d4c024c
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2021-01-29 18:45:01 +0100

    Update Main_interface.py

--- Machine Learning/Main_interface.py
+++ Machine Learning/Main_interface.py
@@ -10,6 +10,7 @@ if not sys.warnoptions:
     warnings.simplefilter("ignore")
     
 import Modeles_ML as ml
+import Modeles_ML_autres as ml_bis
 import Modeles_DL as dl
 import Interface as gui
 import pickle
@@ -24,9 +25,17 @@ filename_DTC_DoH = 'DoH/finalized_model_DTC_DoH.sav'
 filename_RFC_DoH = 'DoH/finalized_model_RFC_DoH.sav'
 filename_XGB_DoH = 'DoH/finalized_model_XGB_DoH.sav'
 
+filename_GNB_DoH = 'DoH/finalized_model_GNB_DoH.sav'
+filename_KNN_DoH = 'DoH/finalized_model_KNN_DoH.sav'
+filename_SVM_DoH = 'DoH/finalized_model_SVM_DoH.sav'
+
 loaded_model_DTC_DoH = pickle.load(open(filename_DTC_DoH, 'rb'))
 loaded_model_RFC_DoH = pickle.load(open(filename_RFC_DoH, 'rb'))
 loaded_model_XGB_DoH = pickle.load(open(filename_XGB_DoH, 'rb'))
+
+loaded_model_GNB_DoH = pickle.load(open(filename_GNB_DoH, 'rb'))
+loaded_model_KNN_DoH = pickle.load(open(filename_KNN_DoH, 'rb'))
+loaded_model_SVM_DoH = pickle.load(open(filename_SVM_DoH, 'rb'))
 
 #Loading DL models for Intrusion
 
@@ -46,7 +55,11 @@ if(button_value == 'Ok'):
     pred_XGB_DoH = ml.XGB_Prediction(df_test, loaded_model_XGB_DoH)
     pred_RFC_DoH = ml.RFC_Prediction(df_test, loaded_model_RFC_DoH)
     
+    pred_GNB_DoH = ml_bis.GNB_Prediction(df_test, loaded_model_GNB_DoH)
+    pred_KNN_DoH = ml_bis.KNN_Prediction(df_test, loaded_model_KNN_DoH)
+    pred_SVM_DoH = ml_bis.SVM_Prediction(df_test, loaded_model_SVM_DoH)
+    
     pred_Simple_DL_Model_Intrusion = dl.DL_simple_Prediction(df_test, loaded_model_Simple_DL_Model_Intrusion)
     
     # Pop up
-    gui.result(pred_DTC_DoH,pred_RFC_DoH,pred_XGB_DoH,pred_Simple_DL_Model_Intrusion)
+    gui.result(pred_DTC_DoH,pred_RFC_DoH,pred_XGB_DoH,pred_Simple_DL_Model_Intrusion, pred_GNB_DoH,pred_KNN_DoH, pred_SVM_DoH)
