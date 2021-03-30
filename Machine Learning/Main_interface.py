2d8842e7cc1a8222ce4a5bb3ddb54c632c6e47a2
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2021-03-30 21:41:47 +0200

    Update Main_interface.py

--- Machine Learning/Main_interface.py
+++ Machine Learning/Main_interface.py
@@ -16,10 +16,13 @@ import Interface as gui
 import pickle
 import pandas as pd
 from keras.models import load_model
+from sklearn import preprocessing
+import Preparation_CSV as pc
+
 
 import Normalisation_donnees as nor
 
-df = pd.read_csv("DoH/df_total_csv_normalisee_DoH.csv", sep=';')
+#df = pd.read_csv("DoH/df_total_csv_normalisee_DoH.csv", sep=';')
 
 #Loading ML models for DoH
 
@@ -35,51 +38,61 @@ loaded_model_DTC_DoH = pickle.load(open(filename_DTC_DoH, 'rb'))
 loaded_model_RFC_DoH = pickle.load(open(filename_RFC_DoH, 'rb'))
 loaded_model_XGB_DoH = pickle.load(open(filename_XGB_DoH, 'rb'))
 
-loaded_model_GNB_DoH = pickle.load(open(filename_GNB_DoH, 'rb'))
-loaded_model_KNN_DoH = pickle.load(open(filename_KNN_DoH, 'rb'))
-loaded_model_SVM_DoH = pickle.load(open(filename_SVM_DoH, 'rb'))
+#loaded_model_GNB_DoH = pickle.load(open(filename_GNB_DoH, 'rb'))
+#loaded_model_KNN_DoH = pickle.load(open(filename_KNN_DoH, 'rb'))
 
 #Loading DL models for Intrusion
 
-filename_Simple_DL_Model_Intrusion = 'Intrusion/finalized_model_Simple_DL_Model_Intrusion.h5'
-loaded_model_Simple_DL_Model_Intrusion = load_model(filename_Simple_DL_Model_Intrusion)
+#filename_Simple_DL_Model_Intrusion = 'Intrusion/finalized_model_Simple_DL_Model_Intrusion.h5'
+#loaded_model_Simple_DL_Model_Intrusion = load_model(filename_Simple_DL_Model_Intrusion)
 
 filename_Conv1D_Model_Intrusion = 'Intrusion/Conv1D.h5'
 filename_Conv2D_Model_Intrusion = 'Intrusion/Conv2D.h5'
-filename_LSTM_Model_Intrusion = 'Intrusion/LSTM.h5'
+#filename_LSTM_Model_Intrusion = 'Intrusion/LSTM.h5'
 
 loaded_model_Conv1D_Model_Intrusion = load_model(filename_Conv1D_Model_Intrusion)
 loaded_model_Conv2D_Model_Intrusion = load_model(filename_Conv2D_Model_Intrusion)
-loaded_model_LSTM_Model_Intrusion = load_model(filename_LSTM_Model_Intrusion)
+#loaded_model_LSTM_Model_Intrusion = load_model(filename_LSTM_Model_Intrusion)
 
 
 # On appelle l'interface principale
-button_value = gui.interface(df,'DoH')
+button_value = gui.interface()
 
 if(button_value == 'Ok'):
 
     df_test = pd.read_csv("df_new.csv", sep=';')
+    
+        
+    #on prepare les données insérées par l'utilisateur  
+    # enlever valeur NaN, transformer IP en int
+    for colonne in df_test.columns:
+        if colonne == 'Timestamp' or colonne == 'TimeStamp' or colonne == 'index' or colonne == 'Label' or colonne == 'Unnamed: 0':
+            del df_test[colonne]
+    
+    
+    df_test = pc.IP2Int(df_test, 'SourceIP')
+    df_test = pc.IP2Int(df_test, 'DestinationIP')
+    df_test = pc.nettoyage(df_test)
     
     # Stocke predictions
     pred_DTC_DoH = ml.DTC_Prediction(df_test, loaded_model_DTC_DoH)
     pred_XGB_DoH = ml.XGB_Prediction(df_test, loaded_model_XGB_DoH)
     pred_RFC_DoH = ml.RFC_Prediction(df_test, loaded_model_RFC_DoH)
     
-    df_test_norm = nor.NormalizeDataset(df_test)
-    pred_GNB_DoH = ml_bis.GNB_Prediction(df_test_norm, loaded_model_GNB_DoH)
-    pred_KNN_DoH = ml_bis.KNN_Prediction(df_test_norm, loaded_model_KNN_DoH)
-    pred_SVM_DoH = ml_bis.SVM_Prediction(df_test_norm, loaded_model_SVM_DoH)
-    
-    pred_Simple_DL_Model_Intrusion = dl.DL_simple_Prediction(df_test, loaded_model_Simple_DL_Model_Intrusion)
+    #df_test_norm = nor.NormalizeDataset(df_test)
+    #pred_GNB_DoH = ml_bis.GNB_Prediction(df_test_norm, loaded_model_GNB_DoH)
+    pred_GNB_DoH=0
+    #pred_KNN_DoH = ml_bis.KNN_Prediction(df_test_norm, loaded_model_KNN_DoH)
+    pred_KNN_DoH = 0
     
     pred_Conv1D_Model_Intrusion = dl.Conv1D_Prediction(df_test, loaded_model_Conv1D_Model_Intrusion)
     pred_Conv2D_Model_Intrusion = dl.Conv2D_Prediction(df_test, loaded_model_Conv2D_Model_Intrusion)
-    pred_LSTM_Model_Intrusion = dl.LSTM_Prediction(df_test, loaded_model_LSTM_Model_Intrusion)
-    
+   
     
     # Pop up
     gui.result(pred_DTC_DoH,pred_RFC_DoH, pred_XGB_DoH, 
-               pred_GNB_DoH,pred_KNN_DoH, pred_SVM_DoH, 
-               pred_Simple_DL_Model_Intrusion,
+               pred_GNB_DoH,pred_KNN_DoH, 
                pred_Conv1D_Model_Intrusion,
-               pred_Conv2D_Model_Intrusion, pred_LSTM_Model_Intrusion)
+               pred_Conv2D_Model_Intrusion)
+    
+ 
