46a021f66d14cafe15a3f43ac773a0df3f65e9aa
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2021-04-05 10:45:34 +0200

    Update Normalisation_donnees.py

--- Machine Learning/Normalisation_donnees.py
+++ Machine Learning/Normalisation_donnees.py
@@ -10,22 +10,21 @@ def normalize(colonne):
     max_value = colonne.max()
     min_value = colonne.min()
     colonne = np.round((colonne - min_value) / (max_value - min_value), 5)
-    return colonne
+    return colonne, min_value, max_value
 
 def NormalizeDataset(dataset) :
     for colonne in dataset.columns:
         if(dataset[colonne].dtypes == 'float64' or dataset[colonne].dtypes == 'int64' and colonne != 'SourcePort' and colonne != 'DestinationPort'):
-           dataset[colonne] = normalize(dataset[colonne])
+           dataset[colonne], _, _ = normalize(dataset[colonne])
            
     return dataset
-
 
 def NormalizeNewValues(original_dataset, new_dataset):
     
     original_dataset = original_dataset.drop(['DoH'], axis = 1)
     for colonne in original_dataset.columns:
         if( colonne != 'SourcePort' and colonne != 'DestinationPort' and original_dataset[colonne].dtypes == 'float64' or original_dataset[colonne].dtypes == 'int64'):
-           print(colonne)
+           
            original_dataset[colonne], min_value, max_value = normalize(original_dataset[colonne])
            
            new_dataset[colonne] = np.round((new_dataset[colonne] - min_value) / (max_value - min_value), 5)
