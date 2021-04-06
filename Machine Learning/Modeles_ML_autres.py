cd8cafbe950c690fe741dd072954a0f2d42f8598
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2021-04-06 12:47:56 +0200

    Update Modeles_ML_autres.py

--- Machine Learning/Modeles_ML_autres.py
+++ Machine Learning/Modeles_ML_autres.py
@@ -17,6 +17,9 @@ import time
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from imblearn.over_sampling import SMOTE
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score, f1_score, recall_score
 
 #Models
 from sklearn.neighbors import KNeighborsClassifier
@@ -39,7 +42,7 @@ def GNB(df, colonne):
     
     X_train, X_test, y_train, y_test = train_test_split(X_smote, y_smote, test_size=0.2, random_state=42)
        
-    GNB = GaussianNB()
+    GNB = GaussianNB(var_smoothing=1e-12)
        
     # Create k neighborsClassoficer
     GNB.fit(X_train,y_train)
@@ -56,9 +59,15 @@ def GNB(df, colonne):
     print("Temps pour GNB (en sec): ", t_total)
 
     
-    #fi.FeaturesImportances(df, colonne, GNB, 'Gaussian Naives Bayes')  
-    roc.ROC_curve(df, colonne, GNB, 'Gaussian Naives Bayes')
-    #cv.cross_validation(df, colonne, GNB, 'Gaussian Naives Bayes')
+    #fi.FeaturesImportances(df, colonne, XGB, 'XGBoost Classifier')  
+    roc.ROC_curve(df, colonne, KNN, 'XGBoost Classifier')
+    #cv.cross_validation(df, colonne, XGB, 'XGBoost Classifier')
+    
+    print("\nMatrice de confusion : \n",confusion_matrix(y_test, y_pred_GNB))
+    print("\nAccuracy : ",accuracy_score(y_test, y_pred_GNB))
+    print("\nPrecision :", precision_score(y_test, y_pred_GNB))
+    print("\nF1 score :", f1_score(y_test, y_pred_GNB))
+    print("\nRecall :", recall_score(y_test, y_pred_GNB))
     
     return GNB
 
@@ -90,7 +99,7 @@ def KNN(df, colonne):
     
     X_train, X_test, y_train, y_test = train_test_split(X_smote, y_smote, test_size=0.2, random_state=42)
        
-    KNN = KNeighborsClassifier(n_neighbors=30)
+    KNN = KNeighborsClassifier(n_neighbors=10)
        
     # Create k neighborsClassoficer
     KNN.fit(X_train,y_train)
@@ -106,16 +115,21 @@ def KNN(df, colonne):
     
     print("Temps pour KNN classifier (en sec): ", t_total)
     
-    #fi.FeaturesImportances(df, colonne, KNN, 'K Neighbors Classifiers')  
-    roc.ROC_curve(df, colonne, KNN, 'K Neighbors Classifiers')
-    #cv.cross_validation(df, colonne, KNN, 'K Neighbors Classifiers')
+    #fi.FeaturesImportances(df, colonne, XGB, 'XGBoost Classifier')  
+    roc.ROC_curve(df, colonne, KNN, 'XGBoost Classifier')
+    #cv.cross_validation(df, colonne, XGB, 'XGBoost Classifier')
+    
+    print("\nMatrice de confusion : \n",confusion_matrix(y_test, y_pred_KNN))
+    print("\nAccuracy : ",accuracy_score(y_test, y_pred_KNN))
+    print("\nPrecision :", precision_score(y_test, y_pred_KNN))
+    print("\nF1 score :", f1_score(y_test, y_pred_KNN))
+    print("\nRecall :", recall_score(y_test, y_pred_KNN))
     
     return KNN
 
 
  
 def KNN_Prediction(df, KNN):    
-
     X_new_KNN = df
     # Predict and print the label for the new data point X_new
     new_prediction_KNN = KNN.predict(X_new_KNN)
@@ -173,3 +187,57 @@ def SVM_Prediction(df, SVM):
     print("New prediction SVM: {}".format(new_prediction_SVM))
     
     return new_prediction_SVM
+
+############################### Perceptron #######################
+    
+####GNb##############
+
+def Perceptron(df, colonne):
+    print('Compiling the Perceptron ...\n')
+    
+    t_debut = time.time()
+    
+    X = df.drop([colonne], axis = 1)
+    y = df[colonne]
+    
+    oversample = SMOTE()
+    X_smote, y_smote = oversample.fit_sample(X, y)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X_smote, y_smote, test_size=0.2, random_state=42)
+       
+    Per =Perceptron(class_weight = 'balanced')
+       
+    Per.fit(X_train,y_train)
+    
+    
+    y_pred_Per = Per.predict(X_test)
+    
+    print('Report GNB \n', classification_report(y_test, y_pred_Per))
+    
+    t_fin = time.time()
+    
+    t_total = t_fin - t_debut
+    
+    print("Temps pour GNB (en sec): ", t_total)
+
+    
+    roc.ROC_curve(df, colonne, KNN, 'XGBoost Classifier')
+    
+    print("\nMatrice de confusion : \n",confusion_matrix(y_test, y_pred_Per))
+    print("\nAccuracy : ",accuracy_score(y_test, y_pred_Per))
+    print("\nPrecision :", precision_score(y_test, y_pred_Per))
+    print("\nF1 score :", f1_score(y_test, y_pred_Per))
+    print("\nRecall :", recall_score(y_test, y_pred_Per))
+    
+    return Per
+
+
+    
+def Perceptron_Prediction(df, Per):    
+
+    X_new_Per = df
+    # Predict and print the label for the new data point X_new
+    new_prediction_Per = Per.predict(X_new_Per)
+    print("New prediction GNB: {}".format(new_prediction_Per))
+    
+    return new_prediction_Per
